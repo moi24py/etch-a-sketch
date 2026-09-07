6439331f3b20678d916140f4c8d706b7369df21b
moi24py <moi24py@gmail.com>
2026-09-07 18:09:56 +0200

    Add basic hover effect to the cells

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const grid = document.querySelector(".grid");
 
 // Make 256 squared divs
 for (let i = 0; i < 256; i++){
-    const div = document.createElement("div");
-    div.setAttribute("style", "width: 6.25%; aspect-ratio: 1; border: 1px solid red; padding: 0; margin: 0; box-sizing: border-box;");
-    grid.appendChild(div);
-}
+    const cell = document.createElement("div");
+    cell.setAttribute("style", "width: 6.25%; aspect-ratio: 1; border: 1px solid red; padding: 0; margin: 0; box-sizing: border-box;");
+    cell.classList.add(".cell");
+    
+    cell.addEventListener("mouseenter", () => {
+        cell.style.backgroundColor = "white";
+    });
 
+    grid.appendChild(cell);
+}
